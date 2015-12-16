eb25df116c46fea7dc4c1fa5babb5688599e5c37
[rtianero] <rTianero@YouSource.Forest>
2015-12-16 14:44:36 +0800

    Added Excel Output

--- MTNDataAnalysis/Chain/WriteToFilesStep.cs
+++ MTNDataAnalysis/Chain/WriteToFilesStep.cs
@@ -8,8 +8,10 @@ namespace MTNDataAnalysis.Chain
 {
     using MTNDataAnalysis.Context;
     using MTNDataAnalysis.Models;
+    using OfficeOpenXml;
     using System;
     using System.Collections.Generic;
+    using System.Data;
     using System.IO;
     using System.Linq;
     using System.Text;
@@ -17,12 +19,18 @@ namespace MTNDataAnalysis.Chain
 
     public class WriteToFilesStep : BaseHandler<CallDataRecordContext>
     {
+        private ExcelPackage package;
+        private ExcelWorkbook workbook;
+        
         private CallDataRecordContext context; 
+
         public override void Process(CallDataRecordContext context)
         {
             this.context = context;
-            WriteCallDataSummary();
-            WriteFileSummary();
+            //WriteCallDataSummary();
+            //WriteFileSummary();
+            WriteToExcel();
+            System.Diagnostics.Process.Start(this.context.OutputPath);
             if (Successor != null)
             {
                 Successor.Process(context);
@@ -46,7 +54,7 @@ namespace MTNDataAnalysis.Chain
 
             fileWriter.Close();
 
-            System.Diagnostics.Process.Start(this.context.OutputPath);
+            
         }
 
         private void WriteFileSummary()
@@ -66,6 +74,108 @@ namespace MTNDataAnalysis.Chain
             fileWriter.Close();
 
             System.Diagnostics.Process.Start(this.context.OutputPath);
+        }
+
+        private void WriteToExcel()
+        {
+            string filename = this.context.OutputPath + "\\Summary_By_" + this.context.GroupByField + "_" + this.context.StartTime.ToString("yyyyMMddhhmmss") + ".xlsx";
+            
+            this.package = new ExcelPackage();
+            this.BuildCallSummarySheet();
+            this.BuildFileSummarySheet();
+            this.package.SaveAs(new FileInfo(filename));
+        }
+
+        private void BuildCallSummarySheet()
+        {
+            ExcelWorksheet worksheet = this.package.Workbook.Worksheets.Add("Call Data Summary");
+            DataTable dataTable = new DataTable();
+
+            worksheet.Cells[1, 1].Value = "Billing Period";
+            worksheet.Cells[1, 2].Value = this.context.GroupByField;
+            worksheet.Cells[1, 3].Value = "Data Volume";
+            worksheet.Cells[1, 4].Value = "In Bytes";
+            dataTable = ConvertToCallDataSummaryDataTable(this.context.CallDataSummary.ToList());
+
+            worksheet.Cells["A2"].LoadFromDataTable(dataTable, false);
+            worksheet.Column(1).AutoFit(0);
+            worksheet.Column(2).AutoFit(0);
+            worksheet.Column(3).AutoFit(0);
+            worksheet.Column(4).AutoFit(0);
+            //Create Header
+        }
+
+        private void BuildFileSummarySheet()
+        {
+            ExcelWorksheet worksheet = this.package.Workbook.Worksheets.Add("File Summary");
+            DataTable dataTable = new DataTable();
+
+            worksheet.Cells[1, 1].Value = "Process Started: ";
+            worksheet.Cells[1, 2].Value = this.context.StartTime.ToString("yyyy-MM-dd hh:mm:ss");
+
+            worksheet.Cells[2, 1].Value = "Process Ended: ";
+            worksheet.Cells[2, 2].Value = this.context.EndTime.ToString("yyyy-MM-dd hh:mm:ss");
+
+            worksheet.Cells[3, 1].Value = "Total Time: ";
+            worksheet.Cells[3, 2].Value = this.context.EndTime.Subtract(this.context.StartTime).TotalSeconds.ToString("#,#.00#") + "Second(s)";
+            
+            worksheet.Cells[5, 1].Value = "Filename";
+            worksheet.Cells[5, 2].Value = "Records Found";
+            worksheet.Cells[5, 3].Value = "Records Processed";
+            worksheet.Cells[5, 4].Value = "Remarks";
+
+            dataTable = ConvertToFileSummaryDataTable(context.FileSummary);
+
+            worksheet.Cells["A6"].LoadFromDataTable(dataTable, false);
+            worksheet.Column(1).AutoFit(0);
+            worksheet.Column(2).AutoFit(0);
+            worksheet.Column(3).AutoFit(0);
+            worksheet.Column(4).AutoFit(0);
+            
+        }
+
+        private DataTable ConvertToFileSummaryDataTable(IList<FileSummary> fileSummary)
+        {
+            var result = new DataTable();
+
+            result.Columns.Add("FileName", typeof(string));
+            result.Columns.Add("RecordsFound", typeof(int));
+            result.Columns.Add("RecordsProcessed", typeof(int));
+            result.Columns.Add("Remarks", typeof(string));
+
+            foreach (var f in fileSummary)
+            {
+                var row = result.NewRow();
+                row["FileName"] = f.FileName;
+                row["RecordsFound"] = f.HeaderCount;
+                row["RecordsProcessed"] = f.RecordsProcessed;
+                row["Remarks"] = f.Success ? "Success" : f.Remarks;
+                result.Rows.Add(row);
+            }
+
+            return result;
+        }
+
+        private DataTable ConvertToCallDataSummaryDataTable(IList<CallDataSummary> callDataSummary)
+        {
+            var result = new DataTable();
+
+            result.Columns.Add("BillingPeriod", typeof(string));
+            result.Columns.Add( context.GroupByField, typeof(string));
+            result.Columns.Add("DataVolume", typeof(string));
+            result.Columns.Add("DataVolumeInBytes", typeof(long));
+
+            foreach (var c in callDataSummary)
+            {
+                var row = result.NewRow();
+                row["BillingPeriod"] = c.BillingPeriod;
+                row[context.GroupByField] = c.GroupingField;
+                row["DataVolume"] = c.HumanReadableSum;
+                row["DataVolumeInBytes"] = c.SumInBytes;
+                result.Rows.Add(row);
+            }
+
+            return result;
         }
     }
 
